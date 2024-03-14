eab7581729a28ad2db4bbd0d36a630439bf1d567
Samuel <muel.h.o33@gmail.com>
2024-03-14 15:32:56 -0300

    teste

--- login.js
+++ login.js
@@ -2,9 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const emailInput = document.getElementById("email");
     const senhaInput = document.getElementById("senha");
     const btnConcluir = document.getElementById("btnConcluir");
-    const video = document.getElementById("background-video")
-
-    video.loop = true;
 
     function verificarPreenchimento() {
         if (emailInput.value.trim() !== "" && senhaInput.value.trim() !== "") {
