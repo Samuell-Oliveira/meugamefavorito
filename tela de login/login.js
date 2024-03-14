7638591d27415ba93f0621ebc849c20207bcb6dc
Samuel <muel.h.o33@gmail.com>
2024-03-14 13:42:33 -0300

    Adicionando a parte de login

---
+++ tela de login/login.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const emailInput = document.getElementById("email");
+    const senhaInput = document.getElementById("senha");
+    const btnConcluir = document.getElementById("btnConcluir");
+
+    function verificarPreenchimento() {
+        if (emailInput.value.trim() !== "" && senhaInput.value.trim() !== "") {
+            btnConcluir.removeAttribute("disabled");
+        } else {
+            btnConcluir.setAttribute("disabled", "disabled");
+        }
+    }
+
+    emailInput.addEventListener("input", verificarPreenchimento);
+    senhaInput.addEventListener("input", verificarPreenchimento);
+});
